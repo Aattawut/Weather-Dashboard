9d39b08538c2d333e8660fd866c4ae1c119c9e42
dhens <dawsonhensel@gmail.com>
2020-01-14 16:17:20 -0800

    not the fully featured update like i mentioned in the last commit, but added icon functionality and search history functionality

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,22 +1,29 @@
 let apiKey = "3ac0d8db34de82819d13a9167239acc1";
 let searchBtn = $(".searchBtn");
 let searchInput = $(".searchInput");
-let searchHistory = [];
 
+// Left column locations
 let cityNameEl = $(".cityName");
 let currentDateEl = $(".currentDate");
-let weatherIconEl = $(".weatherIcon")
+let weatherIconEl = $(".weatherIcon");
+let searchHistoryEl = $(".historyItems");
+
+// Right column locations
 let tempEl = $(".temp");
 let humidityEl = $(".humidity");
 let windSpeedEl = $(".windSpeed");
 let uvIndexEl = $(".uvIndex");
 
+// HTML elements we will append to later
+
+// Create a current date variable
 var today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
-
 var today = mm + '/' + dd + '/' + yyyy;
+
+let searchHistoryArr = [];
 
 
 searchBtn.on("click", function(e) {
@@ -31,22 +38,39 @@ searchBtn.on("click", function(e) {
     })
     .then(function(weatherData) {
         console.log(weatherData);
+        searchHistoryArr.push(citySearchValue);
         let cityObj = {
             cityName: weatherData.name,
             cityTemp: weatherData.main.temp,
             cityHumidity: weatherData.main.humidity,
-            cityWindSpeed: weatherData.wind.speed
+            cityWindSpeed: weatherData.wind.speed,
+            // cityUVIndex: weatherData.
+            cityWeatherIconName: weatherData.weather[0].icon
         }
-        renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed)
+
+        let renderedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+        renderWeatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, renderedWeatherIcon);
+        renderSearchHistory(cityObj.cityName);
     });
 });
 
-function renderWeatherData(cityName, cityTemp, cityHumidity, cityWindSpeed) {
+function renderWeatherData(cityName, cityTemp, cityHumidity, cityWindSpeed, cityWeatherIcon) {
     cityNameEl.text(cityName)
     currentDateEl.text(`(${today})`)
-    tempEl.text(cityTemp);
-    humidityEl.text(cityHumidity);
-    windSpeedEl.text(cityWindSpeed);
+    tempEl.text(`Temperature: ${cityTemp} °F`);
+    humidityEl.text(`Humidity: ${cityHumidity}%`);
+    windSpeedEl.text(`Wind Speed: ${cityWindSpeed} MPH`);
+    uvIndexEl.text();
+    weatherIconEl.attr("src", cityWeatherIcon);
+}
+
+function renderSearchHistory(cityName) {
+    // debugger;
+    let newListItem = $("<li>").attr("class", "historyEntry");
+    for(let i = 0; i < searchHistoryArr.length; i++) {
+        newListItem.text(searchHistoryArr[i]);
+        searchHistoryEl.prepend(newListItem);
+    }
 }
 
 
